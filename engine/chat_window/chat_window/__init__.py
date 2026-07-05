bc03551946583e2000e67a627dd0f60d7f79f758
EXIZ10TION <irsabiev82@.com>
2026-07-05 03:20:58 +0300

    update

--- engine/chat_window/chat_window/__init__.py
+++
@@ -1,7 +0,0 @@
-# Re-export all technical engine modules
-from __future__ import annotations
-from engine.gui.chat_window_pkg.chat_window_engine import config, utils, history_engine, gpt_engine
-from engine.gui.chat_window_pkg.chat_window_engine.config import *
-from engine.gui.chat_window_pkg.chat_window_engine.utils import *
-from engine.gui.chat_window_pkg.chat_window_engine.history_engine import *
-from engine.gui.chat_window_pkg.chat_window_engine.gpt_engine import *
